302d95893dc380612408263d62ff1a0318ba4a33
Brayan <branfer60@gmail.com>
2025-10-02 16:56:19 -0500

    Se inicializo el proyecto

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a>Hola, este es el ejercicio de reforzamiento     
+    </a>
+    </p>
+  </div>
+`
+

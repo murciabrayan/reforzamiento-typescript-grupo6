f91f80c297e8d2dc9337c6ed82a69a11f50463af
Brayan <branfer60@gmail.com>
2025-10-02 17:12:21 -0500

    Ejercicio 7: Desestructuración de notas de un arreglo (primer y segundo elemento)

---
+++ src/ejercicios/07-array-destructuring.ts
@@ -0,0 +1,8 @@
+// Ejercicio 7: Desestructuración de Arreglos
+
+const notasArray: number[] = [4.3, 3.7, 4.9, 4.0];
+
+const [primeraNota, segundaNota] = notasArray;
+
+console.log("Primera nota:", primeraNota);
+console.log("Segunda nota:", segundaNota);
